7209b1bdb6c1006acba409af6904cceddc1f5d9f
MURILO DE OLIVEIRA MARTINS <04198253048@academico.unilasalle.edu.br>
2026-03-10 20:17:48 -0300

    delete user back-end

--- server/src/controllers/userController.ts
+++ server/src/controllers/userController.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from 'express';
-import { getMeService } from '../services/userService';
+import { deleteUserService, getMeService } from '../services/userService';
 
 
 export const getMeController = async (req: Request, res: Response) => {
@@ -14,4 +14,21 @@ export const getMeController = async (req: Request, res: Response) => {
             res.status(400).json({ error: err.message })
     }
     return res.status(500).json({ error: "Server Error" });
-} 
+}
+
+export const deleteUserController = async (req: Request, res: Response) => {
+    try {
+        const userId = req.params.userId
+
+        await deleteUserService(userId as string);
+
+        return res.status(204).send();
+    } catch (err: any) {
+
+        if (err.message === "User doesn't exist.") {
+            return res.status(404).json({ error: err.message })
+        }
+
+        return res.status(500).json({ error: 'Server Error' })
+    }
+}
